375d61971bf69878d453e87e1aadd31d72a90835
Mohamed <qotozer@gmail.com>
2021-12-19 03:26:25 +0200

    implement the react routes to home screen, product screen, cart, login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { render } from 'react-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { Container, Row, Col } from 'react-bootstrap';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route exact path='/' element={<HomeScreen />} />
+            <Route path='/product/:id' element={<ProductScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
