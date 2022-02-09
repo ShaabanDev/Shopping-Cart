7db82722818b6111bc51edb141ad589aad927c03
Mohamed <qotozer@gmail.com>
2022-02-09 22:19:40 +0200

    adding delete product by id route

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,21 +1,27 @@
 import asyncHandler from 'express-async-handler';
 import Product from '../models/productModel.js';
 
-const getProducts =  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.send(products);
-  })
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({});
+  res.send(products);
+});
 
-  const getProductById = asyncHandler(async (req, res) => {
-    try {
-      const product = await Product.findById(req.params.id);
-      res.json(product);
-    } catch (error) {
-      res.status(404).send({ message: 'Product Not Found' });
-    }
-  })
-
-  export {
-      getProducts,
-      getProductById
+const getProductById = asyncHandler(async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.json(product);
+  } catch (error) {
+    res.status(404).send({ message: 'Product Not Found' });
   }
+});
+
+const deleteProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    await product.remove();
+    return res.json({ message: 'Product Removed' });
+  }
+  res.status(404);
+  throw new Error('Product not found');
+});
+export { getProducts, getProductById, deleteProductById };
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import {
+  deleteProductById,
   getProductById,
   getProducts,
 } from '../controllers/productControllers.js';
+import { admin, protect } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
 router.get('/', getProducts);
-
 router.get('/:id', getProductById);
+router.delete('/:id', protect, admin, deleteProductById);
 export default router;
