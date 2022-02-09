da0e35bb0bdd37314c4c83a9e020945c8ad9c4c5
Mohamed <qotozer@gmail.com>
2022-02-09 04:50:25 +0200

    adding update order to paid route

--- controllers/orderControllers.js
+++ controllers/orderControllers.js
@@ -39,4 +39,23 @@ const getOrderById = asyncHandler(async (req, res) => {
   }
   res.send(order);
 });
-export { createOrder, getOrderById };
+
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+  if (!order) {
+    res.status(404);
+    throw new Error('order not found');
+  }
+  order.isPaid = true;
+  order.paidAt = Date.now();
+  order.paymentResult = {
+    id: req.body.id,
+    status: req.body.status,
+    update_time: req.body.update_time,
+    email_address: req.body.email_address,
+  };
+  const updatedOrder = await order.save();
+
+  res.send(updatedOrder);
+});
+export { createOrder, getOrderById, updateOrderToPaid };
--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -1,8 +1,13 @@
 import express from 'express';
-import { createOrder, getOrderById } from '../controllers/orderControllers.js';
+import {
+  createOrder,
+  getOrderById,
+  updateOrderToPaid,
+} from '../controllers/orderControllers.js';
 import protect from '../middleWare/authMiddleWare.js';
 const router = express.Router();
 
 router.post('/', protect, createOrder);
 router.get('/:id', protect, getOrderById);
+router.put('/:id/pay', protect, updateOrderToPaid);
 export default router;
